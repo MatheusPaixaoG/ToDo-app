7009c78749b9351ad3e796b616755739d1beafed
Matheus Paixão <mpgs@cin.ufpe.br>
2022-02-10 15:04:14 -0300

    Criação do componente todo-list

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListHeaderComponent
+    TodoListHeaderComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
