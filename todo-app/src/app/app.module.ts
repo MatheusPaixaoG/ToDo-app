c0aee493ad94deaf370b557c305f15922b827a1a
Matheus Paixão <mpgs@cin.ufpe.br>
2022-02-11 22:04:34 -0300

    Criação do serviço API

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
 import { TodoDataService } from './todo-data.service';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,10 @@ import { TodoDataService } from './todo-data.service';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [TodoDataService],
+  providers: [
+    TodoDataService,
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
